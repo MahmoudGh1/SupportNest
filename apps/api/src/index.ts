dbe28396d901d7d766b583caf2a4eb5887237046
Mahmoud Gharib <MahmoudGH.Business@gmail.com>
2026-06-01 20:30:37 +0300

    edit: modified server configurations

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,29 +1,33 @@
 import { capitalize } from "@supportnest/shared";
 import express from "express";
-import cors from "cors"
-import helmet from "helmet"
-import dotenv from "dotenv"
-import morgan from "morgan"
-import errorHandler from "./middlewares/errorhandler.middleware"
-import notFoundHandler from "./middlewares/notFoundHandler.middleware"
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import errorHandler from "./middlewares/errorhandler.middleware";
+import notFoundHandler from "./middlewares/notFoundHandler.middleware";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(helmet())
-app.use(cors({
-    origin: "https://localhost:5173"
-}))
-app.use(morgan("dev"))
-
+app.use(helmet());
+app.use(
+	cors({
+		origin: "https://localhost:5173",
+	}),
+);
+app.use(morgan("dev"));
 
 app.get("/health", (_, res) => res.json({ ok: true }));
 
-app.use(notFoundHandler)
+app.use(notFoundHandler);
 
-app.use(errorHandler)
-app.listen(3001, () => console.log("API running on 3001"));
+app.use(errorHandler);
+app.listen(PORT, () => {
+	console.log("Server is running on port:", PORT);
+});
