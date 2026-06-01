2b46a40b5384ddfce7bc5472ee74b3aa7af86b2a
Mahmoud Gharib <MahmoudGH.Business@gmail.com>
2026-06-01 20:28:59 +0300

    edit: modified server configurations

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,14 +3,23 @@ import express from "express";
 import cors from "cors"
 import helmet from "helmet"
 import dotenv from "dotenv"
+import morgan from "morgan"
 import errorHandler from "./middlewares/errorhandler.middleware"
 import notFoundHandler from "./middlewares/notFoundHandler.middleware"
 
 dotenv.config()
 
 const app = express();
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
+
+app.use(helmet())
+app.use(cors({
+    origin: "https://localhost:5173"
+}))
+app.use(morgan("dev"))
+
 
 app.get("/health", (_, res) => res.json({ ok: true }));
 
