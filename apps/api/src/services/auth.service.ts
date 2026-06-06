17b89df0db5930370309ec64ce12cde10c116529
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2026-06-06 05:03:13 +0300

    edit: include widgetSecret as part of the response at registration for the business's backend to store and use to sign JWT for customers using the widget

--- apps/api/src/services/auth.service.ts
+++ apps/api/src/services/auth.service.ts
@@ -1,11 +1,26 @@
 import prisma from "src/config/prisma.js";
 import { Role } from "generated/prisma/enums.js";
 import slugify from "src/utils/slug.utils.js";
-import type { LoginInput, OraganizationDataDTO, RegisterInput } from "src/types/auth.types.js";
+import type {
+	LoginInput,
+	OraganizationDataDTO,
+	RegisterInput,
+} from "src/types/auth.types.js";
 import AppError from "src/utils/appError.js";
-import { comparePassword, generateSecret, hashPassword } from "src/utils/password.util.js";
+import {
+	comparePassword,
+	generateSecret,
+	hashPassword,
+} from "src/utils/password.util.js";
 
-export const registerService = async ({ businessName, email, password, firstName, lastName, planId }: RegisterInput) => {
+export const registerService = async ({
+	businessName,
+	email,
+	password,
+	firstName,
+	lastName,
+	planId,
+}: RegisterInput) => {
 	const passwordHash = await hashPassword(password);
 	const widgetSecret = await generateSecret(32);
 	const orgSlug = slugify(businessName);
@@ -39,17 +54,26 @@ export const registerService = async ({ businessName, email, password, firstName
 				id: true,
 				name: true,
 				slug: true,
+				widgetSecret: true,
 			},
 		});
 		return {
-			organization: { id: org.id, name: org.name, slug: org.slug },
+			organization: {
+				id: org.id,
+				name: org.name,
+				slug: org.slug,
+				widgetSecret: org.widgetSecret,
+			},
 		};
 	} catch (err) {
 		throw new AppError("Transaction Failed", 500);
 	}
 };
 
-export const loginService = async ({ email, password }: LoginInput): Promise<OraganizationDataDTO> => {
+export const loginService = async ({
+	email,
+	password,
+}: LoginInput): Promise<OraganizationDataDTO> => {
 	try {
 		const user = await prisma.user.findUnique({ where: { email } });
 		if (!user) {
