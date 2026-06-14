2d98a38141ec1eb896af6760a34612c8e6567a64
Mahmoud Gharib <110766483+MahmoudGh1@users.noreply.github.com>
2026-06-14 06:51:42 +0300

    Update index.ts

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -101,6 +101,6 @@ const Server = createServer(app);
 const wss = new WebSocketServer({ server: Server, path: "/widget/ws" });
 setupWebSocket(wss);
 
-Server.listen(PORT, Number("0.0.0.0"), () => {
+Server.listen(Number(PORT), "0.0.0.0", () => {
 	console.log("Server is running on port:", PORT);
 });
