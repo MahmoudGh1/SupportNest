cdb91e1332a83dec9a780e3c23f8d995c5654385
Mahmoud Gharib <110766483+MahmoudGh1@users.noreply.github.com>
2026-06-20 19:09:00 +0300

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -16,6 +16,14 @@ const nextConfig: NextConfig = {
 			],
 		],
 	},
+	async rewrites() {
+		return [
+			{
+				source: "/api/v1/:path*",
+				destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+			},
+		];
+	},
 };
 
 export default nextConfig;
