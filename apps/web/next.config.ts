944db4961b6a5e3281004b892fdd49d3a2f7834f
Mahmoud Gharib <110766483+MahmoudGh1@users.noreply.github.com>
2026-06-20 19:18:35 +0300

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
 		return [
 			{
 				source: "/api/v1/:path*",
-				destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+				destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`,
 			},
 		];
 	},
